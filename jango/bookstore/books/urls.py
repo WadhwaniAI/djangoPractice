38916878b44c4bf74a39819c2616c5fa45990bdf
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-13 14:28:48 +0530

    generic_view and list_view implemented

--- jango/bookstore/books/urls.py
+++ jango/bookstore/books/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="book.all"),
-    path('<int:id>', views.show, name="book.show"),
+    path('', views.BookListView.as_view(), name="book.all"),
+    path('<int:pk>', views.BookDetailView.as_view(), name="book.show"),
     path('<int:id>/review', views.review, name="book.review")
 ]
  
--- jango/bookstore/books/views.py
+++ jango/bookstore/books/views.py
@@ -1,18 +1,33 @@
 from http.client import HTTPResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from books.models import Book, Review
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
-def index(request):
-    dbData = Book.objects.all()
-    context = {'books': dbData}
-    return render(request, 'books/index.html', context)
+#Create your views here.
+class BookListView(ListView):
+    context_obj_name = 'books'
+    
+    def get_queryset(self):
+        return Book.objects.all()
 
-def show(request, id):
-    singeBook = get_object_or_404(Book, pk=id)
-    review = Review.objects.filter(book_id=id).order_by('-created_at')
-    context = {'book': singeBook, 'reviews':review} 
-    return render(request, 'books/show.html', context) 
+class BookDetailView(DetailView):
+#     review = Review.objects.filter(book_id=id).order_by('-created_at')
+    model = Book
+    pass
+
+
+# def index(request):
+#     dbData = Book.objects.all()
+#     context = {'books': dbData}
+#     return render(request, 'books/index.html', context)
+
+# def show(request, id):
+#     singeBook = get_object_or_404(Book, pk=id)
+#     review = Review.objects.filter(book_id=id).order_by('-created_at')
+#     context = {'book': singeBook, 'reviews':review} 
+#     return render(request, 'books/show.html', context) 
+
+
 
 def review(request, id):
     body = request.POST['review']
