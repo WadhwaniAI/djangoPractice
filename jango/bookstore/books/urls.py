365409d4f61f33c6fae8284cd73efb4a27021139
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-13 15:30:19 +0530

    template inheritance

--- jango/bookstore/books/urls.py
+++ jango/bookstore/books/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name="book.all"),
+    path('', views.index, name="book.all"),
     path('<int:pk>', views.BookDetailView.as_view(), name="book.show"),
     path('<int:id>/review', views.review, name="book.review")
 ]
--- jango/bookstore/books/views.py
+++ jango/bookstore/books/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from books.models import Book, Review
 from django.views.generic import ListView, DetailView
 
 #Create your views here.
-class BookListView(ListView):
-    context_obj_name = 'books'
-    
-    def get_queryset(self):
-        return Book.objects.all()
+def index(request):
+    dbData = Book.objects.all()
+    context = {'books': dbData}
+    return render(request, 'books/book_list.html', context)
 
 class BookDetailView(DetailView):
 #     review = Review.objects.filter(book_id=id).order_by('-created_at')
@@ -21,10 +20,7 @@ class BookDetailView(DetailView):
     pass
 
 
-# def index(request):
-#     dbData = Book.objects.all()
-#     context = {'books': dbData}
-#     return render(request, 'books/index.html', context)
+
 
 # def show(request, id): 
 #     singeBook = get_object_or_404(Book, pk=id)
