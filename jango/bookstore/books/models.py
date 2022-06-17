29bd32320708aab772fb784d2113dd7c25259ecb
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-17 11:28:50 +0530

    connect user with review

--- jango/bookstore/books/models.py
+++ jango/bookstore/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -22,6 +23,7 @@ class Book(models.Model):
     
 class Review(models.Model):
     body = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
     
--- jango/bookstore/bookstore/urls.py
+++ jango/bookstore/bookstore/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("book/", include('books.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True))
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True)),
     path('', include('django.contrib.auth.urls'))
 ]
